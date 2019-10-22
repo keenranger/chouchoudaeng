3be9f294c9a5bc4aefeaf227801c4e2f7113741b
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-10-22 23:45:50 +0900

    button and led done

--- chouchoudaeng.ino
+++ chouchoudaeng.ino
@@ -1,6 +1,6 @@
 boolean dryer_power[2] = {0, 0};
-int button_array[9] = {22, 23, 24, 25, 26, 27, 28, 29, 30};
-int led_array[6] = {40, 41, 42, 43, 44, 45};
+int button_array[9] = {22, 23, 24, 25, 26, 27, 28, 29, 30}; //물받기.샴푸.월풀.스파.헹굼.욕조청소.일시정지.드라이1.드라이2
+int led_array[9] = {40, 41, 42, 43, 44, 45, 46, 47, 48}; //46더미핀
 
 ///////these lines are for debouncing buttons///////////
 boolean last_button_state[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -10,8 +10,17 @@ const int debounce_delay = 50;
 /////////////////////////////////////////////////////////
 
 boolean button_toggle[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int state = 0;//0:일시정지, 1:물받기, 2:샴푸/월풀/스파 3:헹굼 4:욕조청소
 
 void debouncing_button(int i);
+
+void state_0();
+void state_1();
+void state_2(int i);
+void state_3();
+void state_4();
+
+void toggle_reset();
 
 void setup() {
   for (int i = 0; i < 9; i++) { //Set button pins as input
@@ -36,9 +45,84 @@ void debouncing_button(int i) {
   if ((millis() - last_debounce[i]) > debounce_delay) { //스위치 상태가 debounce_delay 이상 같으면
     if (reading != button_state[i]) { //state와 상태가 다를경우 기록
       button_state[i] = reading;
-      if (button_state[i] == HIGH) //HIGH일때 작업 할당
-        button_toggle[i] = ~button_toggle[i];
+      if (button_state[i] == HIGH) { //HIGH일때 작업 할당
+        switch (i) {
+          case 0:
+            state_1();
+            break;
+          case 1 ... 3:
+            state_2(i);
+            break;
+          case 4:
+            state_3();
+            break;
+          case 5:
+            state_4();
+            break;
+          case 6:
+            state_0();
+            break;
+          default:
+            button_toggle[i] = !button_toggle[i];
+            break;
+        }
+      }
     }
   }
   last_button_state[i] = reading;
+}
+void state_1() {
+  toggle_reset();
+  if (state != 1) {// 물받기 킬때
+    state = 1;
+    button_toggle[0] = 1;
+  }
+  else {//물받기 끌때
+
+  }
+}
+
+void state_2(int i) {
+  if (state != 2) {//샴.월.스 처음 킬때
+    toggle_reset();
+    state = 2;
+    button_toggle[i] = 1;
+  }
+  else {//샴.월.스 토글
+    button_toggle[i] = !button_toggle[i];
+  }
+}
+
+void state_3() {
+  toggle_reset();
+  if (state != 3) {//헹굼 킬때
+    state = 3;
+    button_toggle[4] = 1;
+  }
+  else {//헹굼 끌때
+
+  }
+}
+
+void state_4() {
+  toggle_reset();
+  if (state != 4) {//욕조청소 킬때
+    state = 4;
+    button_toggle[5] = 1;
+  }
+  else {//욕조청소 끌때
+
+  }
+}
+
+void state_0() {//정지버튼
+  toggle_reset();
+  state = 0;
+  button_toggle[6] = 1;
+}
+
+void toggle_reset() { //wash쪽 toggle 리셋
+  for (int i = 0; i < 7; i++) {
+    button_toggle[i] = 0;
+  }
 }
